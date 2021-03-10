721512a8e2448b3a2e8defda3a59b43a168c999e
fberrabah <farid.berrabah@gmail.com>
2021-03-10 11:13:27 +0100

    first commit/home/apprenant/anaconda3/bin/python

---
+++ main.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+# coding: utf-8
+from controller import *
+
+
+if __name__=='__main__':
+    
+   
+    print("\033[32m\n----------------------------------------\n\033[0m")
+    print('\033[32mBienvenue dans votre application.\033[0m')
+    print("\033[32m\n----------------------------------------\n\033[0m")
+
+    """boucle qui indique à l'utilisateur ce qu'il souhaite faire et lance les méthodes en fonction des réponses"""
+    choix=""  
+    while choix != "q":
+        choix = input("\033[33m\n(s) Pour créer des groupes.\n(q) Pour quitter.\nVotre choix :\033[0m").lower()
+        if choix == "s": 
+            
+            A = ""
+            lvl = ""
+            while A != "o" and  A != "n":
+                A = input("\033[32m Souhaitez-vous faire des groupes par compétence. (o/n) : \033[0m").lower() 
+
+                if A == "n" :
+                    create_grp()
+                
+                
+                if A == "o" :
+                    comp(lvl,comp)
+
+                
+            
+        if choix == "q":
+            print("A bientôt.")
